a91f62cce294e500e52e87c0918c1cbf79980875
Riku Tsunoda <53454871+2ndr9@users.noreply.github.com>
2024-01-07 09:18:27 +0900

    up setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,16 @@ xcode-select --install
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+chmod +x link.sh brew.sh
+
+sh link.sh
+
 brew doctor
 
 brew update --verbose
 
 brew upgrade --verbose
 
-brew bundle --file ./.Brewfile --verbose
+sh brew.sh
 
 brew cleanup --verbose
